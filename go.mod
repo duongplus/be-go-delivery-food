94e99bab7cef8407174c82ba83ab54b50f9361c0
Duong <duongplus1011@gmail.com>
2022-08-16 09:50:57 +0700

    init gin

---
+++ go.mod
@@ -0,0 +1,11 @@
+module be-go-delivery-food
+
+go 1.16
+
+require (
+	github.com/aws/aws-sdk-go v1.44.64
+	github.com/btcsuite/btcutil v1.0.2 // indirect
+	github.com/gin-gonic/gin v1.8.1
+	gorm.io/driver/mysql v1.3.5
+	gorm.io/gorm v1.23.8
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	dns := os.Getenv("DBConnectionStr")
+
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+
+	fmt.Println(db, "err =>", err)
+
+	if err := runService(db); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func runService(db *gorm.DB) error {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return r.Run()
+}
